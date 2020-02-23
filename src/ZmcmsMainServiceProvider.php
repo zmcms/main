cffd2adf21109841022113393b66136191957603
zmcms <59611848+zmcms@users.noreply.github.com>
2020-02-23 21:33:04 +0100

    Update ZmcmsMainServiceProvider.php

--- src/ZmcmsMainServiceProvider.php
+++ src/ZmcmsMainServiceProvider.php
@@ -17,13 +17,13 @@ class ZmcmsMainServiceProvider extends ServiceProvider{
 		$this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'frontend\routes'.DIRECTORY_SEPARATOR.'web.php');
 		$this->loadMigrationsFrom(__DIR__.'/migrations');
 		$this->publishes([
-			__DIR__.'/backend/views' => base_path('resources/views/themes/'.Config('frontend.theme_name').'/zmcms/backend/main'),
-			__DIR__.'/frontend/views' => base_path('resources/views/themes/'.Config('frontend.theme_name').'/zmcms/frontend/main'),
+			__DIR__.'/backend/views' => base_path('resources/views/themes/zmcms/backend'),
+			__DIR__.'/frontend/views' => base_path('resources/views/themes/zmcms/frontend'),
 			__DIR__.'/config' => base_path('config/zmcms'),
-			__DIR__.'/backend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/css/zmcms/main'),
-			__DIR__.'/frontend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/css/zmcms/main'),
-			__DIR__.'/backend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/js/zmcms/main'),
-			__DIR__.'/frontend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/js/zmcms/main'),
+			__DIR__.'/backend/css' => base_path('public/themes/zmcms/backend/css'),
+			__DIR__.'/frontend/css' => base_path('public/themes/zmcms/frontend/css'),
+			__DIR__.'/backend/js' => base_path('public/themes/zmcms/backend/js'),
+			__DIR__.'/frontend/js' => base_path('public/themes/zmcms/frontend/js'),
 		]);
 	}
 
