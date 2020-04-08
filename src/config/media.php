32258a11a85bf356f318c69155e89cbd2a0577c9
zmcms <59611848+zmcms@users.noreply.github.com>
2020-04-08 02:15:06 +0200

    Add files via upload

---
+++ src/config/media.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Ten plik został utworzony automatycznie.
+* Jeżeli wiesz, co robisz, możesz edytować samodzielnie ten plik, 
+* jednak zalecamy mocno użycie formularza do aktualizacji tych danych w systemie
+* Odpowiednie opcje znajdziesz w sekcji "Ustawienia"
+**/
+
+return array (
+  'folder' => '/themes/zmcms/media/',
+  'logo' => '/themes/zmcms/media/site/logo.png',
+  'icon' => '/themes/zmcms/media/site/icon.png',
+  'img' => 
+  array (
+    'sizes' => 
+    array (
+      0 => 15,
+      1 => 30,
+      2 => 100,
+      3 => 200,
+      4 => 300,
+      5 => 600,
+      6 => 800,
+      7 => 1024,
+      8 => 1400,
+      9 => 1980,
+    ),
+    'types' => 
+    array (
+    ),
+    'folder' => '/themes/zmcms/media/img/',
+  ),
+);
