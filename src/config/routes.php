b39f1c35ec3003d3093bdf6da2a01c458e3585ae
zmcms <59611848+zmcms@users.noreply.github.com>
2020-12-07 22:33:34 +0100

    Update routes.php

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,1 +1,30 @@
-routes.php
+<?php
+/**
+ * KOLEJNOŚĆ PLIKÓW Z ROUTINGAMI SYSTEMU
+ * Wszystkie routingi powinny być dostarczone przez modułe z katalogu vendor
+ * Ważna jest kolejność dołączania tych plików
+ */
+$d = DIRECTORY_SEPARATOR;
+return [
+	'backend'=>[
+		'zmcms'.$d.'main'.$d.'src'.$d.'backend'.$d.'routes'.$d.'web.php',
+		'zmcms'.$d.'main'.$d.'src'.$d.'backend'.$d.'routes'.$d.'console.php',
+		'zmcms'.$d.'users'.$d.'src'.$d.'backend'.$d.'routes'.$d.'users.php',
+		'zmcms'.$d.'users'.$d.'src'.$d.'backend'.$d.'routes'.$d.'users_console.php',
+		'zmcms'.$d.'website_articles'.$d.'src'.$d.'backend'.$d.'routes'.$d.'website_articles.php',
+		'zmcms'.$d.'website_articles'.$d.'src'.$d.'backend'.$d.'routes'.$d.'website_articles_console.php',
+		'zmcms'.$d.'website_navigations'.$d.'src'.$d.'backend'.$d.'routes'.$d.'website_navigations.php',
+		'zmcms'.$d.'website_navigations'.$d.'src'.$d.'backend'.$d.'routes'.$d.'website_navigations_console.php',
+	
+
+	],
+	'frontend'=>[
+		'zmcms'.$d.'main'.$d.'src'.$d.'frontend'.$d.'routes'.$d.'web.php',
+		'zmcms'.$d.'users'.$d.'src'.$d.'frontend'.$d.'routes'.$d.'users.php',
+		'zmcms'.$d.'website_articles'.$d.'src'.$d.'frontend'.$d.'routes'.$d.'website_articles.php',
+		'zmcms'.$d.'website_navigations'.$d.'src'.$d.'frontend'.$d.'routes'.$d.'website_navigations.php',
+
+		
+	],
+];
+
