501b0fab5070e278bb5e4aff44369bae9e49c2e6
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-16 19:53:39 +0200

    Update main_offer.blade.php

--- src/frontend/views/home/parts/main_offer.blade.php
+++ src/frontend/views/home/parts/main_offer.blade.php
@@ -1,14 +1,6 @@
 <header>
-	<h1>{{Config(Config('zmcms.frontend.theme_name').'.contact_data.headquarters.business_name')}}</h1>
+	<h1>{{ Config(Config('zmcms.frontend.theme_name').'.contact_data.headquarters.business_name') ?? null }}</h1>
 </header>
 <ul class="home">
-	{{zmcms_website_navigations_frontend($position = 'sects', $parent = null, $to_file=false)}}
-	{{zmcms_website_navigations_frontend($position = 'addictions', $parent = null, $to_file=false)}}
+	{{zmcms_website_navigations_frontend($position = 'home', $parent = null, $to_file=false)}}
 </ul>
-<content>
-<blockquote class="pc_6">Niesiemy pomoc w &nbsp;rozwiązywaniu problem&oacute;w wynikających z uzależnień od środk&oacute;w psychoaktywnych.</blockquote>
-<blockquote class="pc_6">Niesiemy pomoc osobom uzależnionym oraz członkom ich rodzin.</blockquote>
-<blockquote class="pc_6">Szerzymy wiedzę o istocie uzależnień i możliwościach terapii.</blockquote>
-<blockquote class="pc_6">Prowadzimy profilaktykę uzależnień, psychomanipulacji, zachowań agresywnych, kompulsywnych oraz indoktrynacji psychicznej.</blockquote>
-</content>
-<p>&nbsp;</p>
