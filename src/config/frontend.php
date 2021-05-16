b381ec857e820a135cfe692a0a3bd06d4156bea4
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-16 18:47:33 +0200

    Update frontend.php

--- src/config/frontend.php
+++ src/config/frontend.php
@@ -40,53 +40,6 @@ return [
 		* 	[
 		* 		'position'=>'start_page', 
 		* 		'q'=>6, 
-		* 		'view'=>'themes.autofutura.frontend.home.parts.main_offer',
-		* 	],
-		* ],
-		* ===========================================================================================
-		**/
-		<?php
-$d=DIRECTORY_SEPARATOR;
-/**
- * NIE DOTYKAĆ PONIŻSZYCH DWÓCH WERSÓW
- */
-if(function_exists('base_path'))
-$mod_path = base_path().DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'zmcms'.DIRECTORY_SEPARATOR;
-
-return [
-	/**
-	 * DOMYŚLNA BAZA DANYCH APLIKACJI
-	 * W przyszłości system będzie mógł przełączać się pomiędzy lustrzanymi bazami.
-	 * W przypadku awarii bazy podstawowej system automatycznie przełączy się na 
-	 * bazę z klucza 'database_mirror'.
-	 */
-	// 'database_main'=>	'zmcms', // 
-	/**
-	 * ZAPASOWA BAZA DANYCH APLIKACJI
-	 */
-	// 'database_mirror'=>	'zmcms',
-	/**
-	 * Poniżej nazwa "tematu", "skórki" itp.
-	 */
-	'theme_name'=>			'zmcms',
-	/**
-	 * Tablica, na podstawie której budujemy strukturę strony startowej,
-	 * pobieramy dane, generujemy widoki itp.
-	 * Najlepiej przyjąć jakąś konwencję nazewnictwa tych helperów, 
-	 * np.: zmcms_main_cośtam, zmcms_website_navigations_cotamkolejnego itp., 
-	 * czyli wiemy, jaki jest vendor/pakiet.
-	 * Kolumna 1 to nazwa funkcji - helpera
-	 * Druga kolumna to tablica parametrów wywołaniafunkcji
-	 */
-	'home_page_modules'	=>	[
-		/********************************************************************************************
-		* PRZYKŁADOWE WYWOŁANIE KLASY NA STRONIE STARTOWEJ
-		* ===========================================================================================
-		* [	
-		* 	'Zmcms\WebsiteNavigations\Frontend\Controllers\ZmcmsWebsiteNavigationsController@home', 
-		* 	[
-		* 		'position'=>'start_page', 
-		* 		'q'=>6, 
 		* 		'view'=>'themes.zmcms.frontend.home.parts.main_offer',
 		* 	],
 		* ],
