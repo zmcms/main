37771f4e938ec71c110e664d86d6a1751f3e697a
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-16 19:58:29 +0200

    Update main_offer.blade.php

--- src/frontend/views/home/parts/main_offer.blade.php
+++ src/frontend/views/home/parts/main_offer.blade.php
@@ -2,5 +2,5 @@
 	<h1>{{ Config(Config('zmcms.frontend.theme_name').'.contact_data.headquarters.business_name') ?? null }}</h1>
 </header>
 <ul class="home">
-	{{zmcms_website_navigations_frontend($position = 'home', $parent = null, $to_file=false)}}
+	{{zmcms_website_navigations_frontend($position = 'start', $parent = null, $to_file=false)}}
 </ul>
