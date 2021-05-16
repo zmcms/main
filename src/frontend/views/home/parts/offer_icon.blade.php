7b3d26bae1c2185c2fd34f61b258136d134ff886
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-16 20:36:24 +0200

    Delete offer_icon.blade.php

--- src/frontend/views/home/parts/offer_icon.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach($resultset as $r)
-<a href="{{$r->link}}">
-	<div class="offer_icon">
-		<img src="{{(json_decode($r->images_resized, true)['ilustration']['300'])}}" alt="{{$r->name}}" >
-		<h1>{{$r->name}}</h1>
-	</div>
-</a>
-@endforeach
