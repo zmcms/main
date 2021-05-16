79b791877c571e09df441a004bdc091c300ef40a
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-16 23:46:06 +0200

    Add files via upload

--- src/frontend/views/home/home.blade.php
+++ src/frontend/views/home/home.blade.php
@@ -1,4 +1,11 @@
 @extends('themes.'.(Config('zmcms.frontend.theme_name') ?? 'zmcms').'.frontend.main')
 @section('content')
 	@include('themes.'.(Config('zmcms.frontend.theme_name') ?? 'zmcms').'.frontend.home.parts.main_offer')
+	@foreach(zmcms_website_homepage_categories($position = 'start', $order = ['sort' => 'asc']) as $r)
+		@includeIf('themes.'.(Config('zmcms.frontend.theme_name') ?? 'zmcms').'.frontend.home.parts.'.$r->type)
+	@endforeach
+	{{-- <pre>{{
+		print_r(zmcms_website_homepage_categories($position = 'start', $order = ['sort' => 'asc']), true)
+	}}</pre> --}}
+
 @endsection
